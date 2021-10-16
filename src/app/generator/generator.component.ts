5518e9381b602417a01b1ef648a4f370e10b0751
Miguel Branco <mig.jcb@gmail.com>
2021-10-16 13:54:46 +0100

    Numerous UI improvements, especially to generator page
    - Don't time off bias input if generation is not live
    - If generation is live, bias change triggers regeneration
    - Added clock image and spread out generator input/button
    - Expanded tables to 100% width
    - Extracted the main color to a CSS variable and used it more often

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -33,14 +33,32 @@ export class GeneratorComponent implements OnInit {
     /**
      * Reacts to bias input changes.
      *
+     * When entering a new bias, if the code generation is already running, we refresh
+     * the grid immediately (instead of waiting for the next cycle). We also disable the field
+     * for 4 seconds to prevent bias spamming.
+     *
      * @param newValue The new value.
      */
     onModelChange(newValue: string) {
         if (this.codeService.possibleChars.indexOf(newValue) !== -1) {
-            this.biasDisabled = true;
-            setTimeout(() => this.biasDisabled = false, 4000);
+            if (this.codeService.code) {
+                // Grid generation is already running
+                this.codeService.triggerGeneration(this.bias);
+                this.biasDisabled = true;
+                setTimeout(() => this.biasDisabled = false, 4000);
+            }
         } else {
+            // Discard unrecognized characters by clearing bias after Angular's data binding
             setTimeout(() => this.bias = '', 0);
         }
+    }
+
+    /**
+     * Starts generating codes, or changes the bias character and refreshes the grid.
+     *
+     * @see {@link CodeService.triggerGeneration}
+     */
+    triggerGeneration() {
+        this.codeService.triggerGeneration(this.bias);
     }
 }
